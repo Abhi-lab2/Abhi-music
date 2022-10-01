428f20c7ca065e0e8685c0537583f0e0d4afb7b8
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-10-02 00:38:25 +0530

    Update SongDetails.jsx

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,12 @@
-const SongDetails = () => <div>SongDetails</div>;
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom'
+
+const SongDetails = () =>{
+    const dispatch = useDispatch()
+    return(
+        <div>SongDetails</div>;
+    )
+} 
 
 export default SongDetails;
