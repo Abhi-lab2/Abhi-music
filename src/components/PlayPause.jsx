57d4b7dcfc4ddcd2e1a44ec26b1251da3b3f1114
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-10-03 00:03:04 +0530

    play pause button updated

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,19 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import React from 'react';
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
+
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => (isPlaying && activeSong?.title === song.title ? (
+  // if playing then will show pause icon or will show play(2nd icon).
+  <FaPauseCircle
+    size={35}
+    className="text-gray-300"
+    onClick={handlePause}
+  />
+) : (
+  <FaPlayCircle
+    size={35}
+    className="text-gray-300"
+    onClick={handlePlay}
+  />
+));
 
 export default PlayPause;
